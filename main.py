db79cdda020e731b85419f3761fabf66bff669be
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-10 15:12:30 -0700

    Update and rename dumm.py to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import rpy2
 import json, csv, requests, statistics, bs4
 from urllib.request import urlopen
 import pandas as pd
+import matplotlib.pyplot as plt
 url = "https://codeforces.com/api/user.ratedList?activeOnly=true&includeRetired=false"
 response = urlopen(url)
 data_json = json.loads(response.read())
@@ -9,6 +10,14 @@ results = data_json['result']
 df = pd.DataFrame(results)
 df.to_csv('users.csv')
 
+c = 0
+tot = []
 for index, row in df.iterrows() :
-    if row['rating'] >= 3000 :
-        print(row)
+    tot.append(row)
+tot = pd.DataFrame(tot)
+arr = []
+for x in tot['maxRating'] :
+    arr.append(x)
+plt.hist(arr, bins=1000)
+plt.show()
+print(statistics.mode(arr))
