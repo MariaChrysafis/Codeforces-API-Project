7800697f070396837d53a2d2dc3cb49d6ff94be1
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-10 12:25:39 -0700

    Update dumm.py

--- dumm.py
+++ dumm.py
@@ -1,10 +1,11 @@
-import json
-import csv
-import requests
+import json, csv, requests, statistics, bs4
 from urllib.request import urlopen
-import statistics
-import bs4
 import pandas as pd
 url = "https://codeforces.com/api/problemset.problems?tags=implementation"
 response = urlopen(url)
 data_json = json.loads(response.read())
+results = data_json['result']
+problems = results['problems']
+print(problems)
+# employee_data = data['results']
+
