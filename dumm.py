297eb3ae9617e92c24ef237308acaeac53e6a1c9
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-10 13:02:38 -0700

    Update dumm.py

--- dumm.py
+++ dumm.py
@@ -2,13 +2,6 @@ import rpy2
 import json, csv, requests, statistics, bs4
 from urllib.request import urlopen
 import pandas as pd
-url = "https://codeforces.com/api/problemset.problems?tags=implementation"
-response = urlopen(url)
-data_json = json.loads(response.read())
-results = data_json['result']
-problems = results['problems']
-df = pd.DataFrame(problems)
-df.to_csv('problems.csv')
 url = "https://codeforces.com/api/user.ratedList?activeOnly=true&includeRetired=false"
 response = urlopen(url)
 data_json = json.loads(response.read())
