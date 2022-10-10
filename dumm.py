6d10af4cce19f0f9016ac6c74f4442a08c3fb396
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-10 12:35:20 -0700

    Update dumm.py

--- dumm.py
+++ dumm.py
@@ -1,3 +1,4 @@
+import rpy2
 import json, csv, requests, statistics, bs4
 from urllib.request import urlopen
 import pandas as pd
@@ -7,5 +8,6 @@ data_json = json.loads(response.read())
 results = data_json['result']
 problems = results['problems']
 print(problems)
-# employee_data = data['results']
-
+print(type(problems))
+df = pd.DataFrame(problems)
+df.to_csv('problems.csv')
